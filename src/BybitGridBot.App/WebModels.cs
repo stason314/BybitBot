255d5877bb43480f0b9e78732d7418565b29fbae
Stas <Klukhin>
2026-05-17 14:43:24 +0300

    Fix futures market scan score fields

--- src/BybitGridBot.App/WebModels.cs
+++ src/BybitGridBot.App/WebModels.cs
@@ -572,6 +572,16 @@ public sealed class FuturesMarketScanItem
 
     public required string StrategyFitName { get; init; }
 
+    public decimal RangeQualityScore { get; init; }
+
+    public decimal BreakoutQualityScore { get; init; }
+
+    public decimal DumpRiskScore { get; init; }
+
+    public decimal FeeEfficiencyScore { get; init; }
+
+    public decimal LiquidityScore { get; init; }
+
     public decimal GridFitScore { get; init; }
 
     public decimal TrendFitScore { get; init; }
